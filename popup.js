79ba2ab949d713d270fbfc3a6aac93133fae35cb
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-10-11 13:36:46 -0700

    add pan and gain controls

--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
 const gainrange = document.querySelector("#gaindisplay");
+const panrange = document.querySelector("#pandisplay");
 
 document.querySelector("#gain").addEventListener("input", (e) => {
     gainrange.textContent = parseFloat(e.target.value).toFixed(2);
+});
+
+document.querySelector("#pan").addEventListener("input", (e) => {
+    panrange.textContent = parseFloat(e.target.value).toFixed(2);
 });
