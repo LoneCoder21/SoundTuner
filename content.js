676e1656c094a71b0237bf8cea34cf056ae05d3c
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-10-11 22:17:06 -0700

    passing messages from popup to content

---
+++ content.js
@@ -0,0 +1,10 @@
+(async () => {
+    if (window.hasRun) {
+        return;
+    }
+    window.hasRun = true;
+
+    browser.runtime.onMessage.addListener((message) => {
+        console.log(message);
+    });
+})();
--- popup.js
+++ popup.js
@@ -1,10 +1,43 @@
-const gainrange = document.querySelector("#gaindisplay");
-const panrange = document.querySelector("#pandisplay");
+document.addEventListener("DOMContentLoaded", () => {
+    const gainrange = document.querySelector("#gaindisplay");
+    const panrange = document.querySelector("#pandisplay");
 
-document.querySelector("#gain").addEventListener("input", (e) => {
-    gainrange.textContent = parseFloat(e.target.value).toFixed(2);
-});
+    document.querySelector("#gain").addEventListener("input", (e) => {
+        gainrange.textContent = parseFloat(e.target.value).toFixed(2);
+    });
 
-document.querySelector("#pan").addEventListener("input", (e) => {
-    panrange.textContent = parseFloat(e.target.value).toFixed(2);
+    document.querySelector("#pan").addEventListener("input", (e) => {
+        panrange.textContent = parseFloat(e.target.value).toFixed(2);
+    });
+
+    document.querySelector("#gain").addEventListener("change", (e) => {
+        sendMessageAllTabs();
+    });
+
+    document.querySelector("#pan").addEventListener("change", (e) => {
+        sendMessageAllTabs();
+    });
+
+    function sendMessageAllTabs() {
+        console.log("sending...");
+        browser.tabs
+            .query({})
+            .then((tabs) => {
+                for (const tab in tabs) {
+                    const id = tabs[tab].id;
+                    console.log(id);
+
+                    browser.tabs
+                        .sendMessage(id, {
+                            greeting: "Hi from background script",
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 });
