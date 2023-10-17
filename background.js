5b54c37f4a265ca6626e5073e9773795c4b2d373
Abhinav Neelam <abhinavneelam949@gmail.com>
2023-10-17 12:31:33 -0700

    bug fix on cache data + auto loading data on content script

--- background.js
+++ background.js
@@ -5,17 +5,34 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === "setSession") {
         const { tabid, data } = request;
         session[tabid] = data;
+        return false;
     } else if (request.type === "getSession") {
         const { tabid } = request;
         const data = session[tabid];
         sendResponse(data);
+        return true;
     }
 
     //message from a content script
     if (sender.envType === "content_child") {
         if (request.type === "getInitialData") {
-            const data = session[sender.tab.id];
-            sendResponse(data);
+            const session_data = session[sender.tab.id];
+            let url = sender.tab.url;
+            const domain = new URL(url).hostname;
+            url = url.replace(/(^\w+:|^)\/\//, ""); // strip protocol
+            url = url.split("#")[0]; // strip hashes
+
+            const store = browser.storage.local.get(domain).then((store) => {
+                const domaindata = store[domain]?.["hostname"];
+                const fulldata = store[domain]?.[url];
+
+                let cache = fulldata ? fulldata : domaindata;
+                cache = cache ? cache : { gain: 1.0, pan: 0.0, mono: false };
+
+                const data = session_data ? session_data : cache;
+                sendResponse({ ...data });
+            });
         }
     }
+    return true;
 });
--- popup.js
+++ popup.js
@@ -52,7 +52,8 @@ document.addEventListener("DOMContentLoaded", () => {
         const url = await getUrl();
         const domain = url[0];
         const fullurl = url[1];
-        const store = await browser.storage.local.get(domain);
+        const all_store = await browser.storage.local.get(domain);
+        const store = all_store[domain];
         const l = await browser.storage.local.set({
             [domain]: { ...store, hostname: { ...data } },
         });
@@ -62,7 +63,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const url = await getUrl();
         const domain = url[0];
         const fullurl = url[1];
-        const store = await browser.storage.local.get(domain);
+        const all_store = await browser.storage.local.get(domain);
+        const store = all_store[domain];
+
         const l = await browser.storage.local.set({
             [domain]: { ...store, [fullurl]: { ...data } },
         });
@@ -86,10 +89,8 @@ document.addEventListener("DOMContentLoaded", () => {
             tabid,
         });
 
-        let settings = cache
-            ? { ...cache }
-            : { gain: 1.0, pan: 0.0, mono: false };
-        settings = response ? { ...response } : settings;
+        let settings = cache ? cache : { gain: 1.0, pan: 0.0, mono: false };
+        settings = response ? response : settings;
         applySettings(settings);
     }
     loadSettings();
